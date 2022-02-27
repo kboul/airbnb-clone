b6d159b2f75d4511d7afc235c836a004473b5596
kboul <kos.voulgaridis@gmail.com>
2022-02-27 15:36:04 +0200

    Create banner text, button and include on click animation

--- components/Banner/Banner.jsx
+++ components/Banner/Banner.jsx
@@ -1,15 +1,17 @@
 import Image from "next/image";
 
 import Styled from "./styles";
+import bannerImage from "./airbnb-banner.webp";
 
 export default function Banner() {
   return (
-    <Styled.ImageContainer>
-      <Image
-        layout="fill"
-        objectFit="contain"
-        src="https://links.papareact.com/0fm"
-      />
-    </Styled.ImageContainer>
+    <Styled.Container>
+      <Image layout="fill" objectFit="cover" priority src={bannerImage} />
+
+      <Styled.BannerTextContainer>
+        <Styled.BannerText>Not sure where to go? Perfect.</Styled.BannerText>
+        <Styled.FlexibleButton>I’m flexible</Styled.FlexibleButton>
+      </Styled.BannerTextContainer>
+    </Styled.Container>
   );
 }
--- components/Banner/styles.js
+++ components/Banner/styles.js
@@ -1,6 +1,6 @@
 import styled from "styled-components";
 
-const ImageContainer = styled.div`
+const Container = styled.div`
   position: relative;
   display: flex;
   height: 300px;
@@ -36,4 +36,47 @@ const ImageContainer = styled.div`
   }
 `;
 
-export default { ImageContainer };
+const BannerTextContainer = styled.div`
+  position: absolute;
+  top: 62%;
+  width: 100%;
+  text-align: center;
+`;
+
+const BannerText = styled.p`
+  color: #ffffff;
+  font-size: 32px;
+  font-weight: 600;
+  letter-spacing: -0.02em;
+  line-height: 36px;
+
+  @media (max-width: 576px) {
+    font-size: 1.125rem; /* 18px */
+    line-height: 1.75rem; /* 28px */
+  }
+`;
+
+const FlexibleButton = styled.button`
+  color: rgb(168 85 247);
+  border: 0px;
+  background-color: #f7f7f7;
+  padding-left: 2.5rem; /* 40px */
+  padding-right: 2.5rem; /* 40px */
+  padding-top: 1rem; /* 16px */
+  padding-bottom: 1rem; /* 16px */
+  border-radius: 9999px;
+  box-shadow: 0 4px 6px -1px rgb(0 0 0 / 0.1), 0 2px 4px -2px rgb(0 0 0 / 0.1);
+  font-weight: bold;
+  cursor: pointer;
+
+  &:active {
+    transform: scale(0.9);
+    transition-property: color, background-color, border-color,
+      text-decoration-color, fill, stroke, opacity, box-shadow, transform,
+      filter, backdrop-filter;
+    transition-timing-function: cubic-bezier(0.4, 0, 0.2, 1);
+    transition-duration: 150ms;
+  }
+`;
+
+export default { BannerText, BannerTextContainer, Container, FlexibleButton };
