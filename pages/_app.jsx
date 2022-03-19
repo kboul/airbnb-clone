c1a917a0f8b6852966b23eb790c945eb17747749
Konstantinos Voulgaridis <kos.voulgaridis@gmail.com>
2022-03-19 22:00:32 +0200

    Introduce loading indicator on route change (#11)

--- pages/_app.jsx
+++ pages/_app.jsx
@@ -1,7 +1,18 @@
 import PropTypes from "prop-types";
 import { createGlobalStyle } from "styled-components";
+import ProgressBar from "@badrap/bar-of-progress";
+import Router from "next/router";
 
-// import "./globals.css";
+const progress = new ProgressBar({
+  size: 4,
+  color: "#FE595E",
+  className: "z-50",
+  delay: 100
+});
+
+Router.events.on("routeChangeStart", progress.start);
+Router.events.on("routeChangeComplete", progress.finish);
+Router.events.on("routeChangeError", progress.finish);
 
 export default function MyApp({ Component, pageProps }) {
   return (
